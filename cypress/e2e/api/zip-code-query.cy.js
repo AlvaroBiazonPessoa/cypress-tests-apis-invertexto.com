58d6acd027a305fc41bb65accfc5eac18e0d9af4
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2025-04-24 20:49:12 -0300

    refactor: Adicionando constante para ser reutilizada nos casos de testes/blocos it

--- cypress/e2e/api/zip-code-query.cy.js
+++ cypress/e2e/api/zip-code-query.cy.js
@@ -1,5 +1,7 @@
 describe('ZIP Code Query API', () => {
 
+    const httpMethodGet = 'GET'
+
     it('Return ZIP code data with an unexpected HTTP method', () => {
         const unexpectedHttpMethod = 'POST'
         const failOnStatusCode = false
@@ -15,26 +17,24 @@ describe('ZIP Code Query API', () => {
     })
 
     it('Return ZIP code data without sending ZIP code parameter', () => {
-        const httpMethod = 'GET'
         const failOnStatusCode = false
         const apiToken = Cypress.env('TOKEN_FOR_ALL_APIS')
         const authorization = `Bearer ${apiToken}`
         const zipCode = null
         const allUrl = `cep/${zipCode}`
         const httpStatusUnprocessableEntity = 422
-        cy.api_returnZipCodeData(httpMethod, allUrl, failOnStatusCode, authorization)
+        cy.api_returnZipCodeData(httpMethodGet, allUrl, failOnStatusCode, authorization)
             .then((response) => {
                 expect(response.status).to.eq(httpStatusUnprocessableEntity)
             })
     })
 
     it('Returns ZIP code data without authentication', () => {
-        const httpMethod = 'GET'
         const failOnStatusCode = false
         const zipCode = '09691000'
         const allUrl = `cep/${zipCode}`
         const httpStatusUnauthorized = 401
-        cy.api_returnZipCodeDataWithoutAuthentication(httpMethod, allUrl, failOnStatusCode)
+        cy.api_returnZipCodeDataWithoutAuthentication(httpMethodGet, allUrl, failOnStatusCode)
             .then((response) => {
                 expect(response.status).to.eq(httpStatusUnauthorized)
             })
