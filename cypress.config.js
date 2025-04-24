3491e277fe350288147c05e2236c6767199a59e7
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2025-04-24 18:57:51 -0300

    fix: Ajustando posição do objeto baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  baseUrl: 'https://api.invertexto.com/v1/',
   e2e: {
+    baseUrl: 'https://api.invertexto.com/v1/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
