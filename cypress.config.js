482128bc24e2e3912e8ebcee4314d98c22bbba69
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2025-04-24 18:48:15 -0300

    feat: Adicionando URL base

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  baseUrl: 'https://api.invertexto.com/v1/',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
