cb063b6ee265a84454271d38bf1f7513ffbd0ac9
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2025-04-27 14:18:10 -0300

    refactor: Alterando nome das constantes e variáveis

--- cypress/e2e/api/zip-code-query.cy.js
+++ cypress/e2e/api/zip-code-query.cy.js
@@ -3,24 +3,22 @@ describe('ZIP Code Query API', () => {
     const endpointCep = 'cep/' 
     const httpMethodGet = 'GET'
     let failOnStatusCode = false
+    const zipCodeQueryApiToken = Cypress.env('ZIP_CODE_QUERY_API_TOKEN')
+    const authorizationForTheZipCodeQueryApi = `Bearer ${zipCodeQueryApiToken}`
     const httpStatusUnauthorized = 401
     const statusTextUnauthorized = 'Unauthorized'
     const messageObject = 'message'
     const massageUnauthenticated = 'Unauthenticated.'
     const httpStatusUnprocessableEntity = 422
     const statusTextUnprocessableEntity = 'Unprocessable Entity'
-    const messageUnprocessableEntity = 'O campo cep é obrigatório.'
-    let requestResponseBodyMessage = ''
 
     it('Return ZIP code data with an unexpected HTTP method', () => {
-        const unexpectedHttpMethod = 'POST'
-        const apiToken = Cypress.env('TOKEN_FOR_ALL_APIS')
-        const authorization = `Bearer ${apiToken}`
-        const zipCode = '09691000'
+        const HttpMethodPost = 'POST'
+        const zipCode = '01001000'
         const url = endpointCep + zipCode
         const httpStatusNotAllowed = 405
         const statusTextNotAllowed = 'Method Not Allowed'
-        cy.api_returnZipCodeData(unexpectedHttpMethod, url, failOnStatusCode, authorization)
+        cy.api_returnZipCodeData(HttpMethodPost, url, failOnStatusCode, authorizationForTheZipCodeQueryApi)
             .then((response) => {
                 expect(response.status).to.eq(httpStatusNotAllowed)
                 expect(response.statusText).to.eq(statusTextNotAllowed)
@@ -28,7 +26,7 @@ describe('ZIP Code Query API', () => {
     })
 
     it('Returns ZIP code data without authentication', () => {
-        const zipCode = '09691000'
+        const zipCode = '30130010'
         const url = endpointCep + zipCode
         cy.api_returnZipCodeDataWithoutAuthentication(httpMethodGet, url, failOnStatusCode)
             .then((response) => {
@@ -40,11 +38,11 @@ describe('ZIP Code Query API', () => {
     })
 
     it('Returns ZIP code data with an invalid token', () => {
-        const invalidApiToken = Cypress.env('INVALID_TOKEN')
-        const authorization = `Bearer ${invalidApiToken}`
-        const zipCode = '09691000'
+        const invalidToken = Cypress.env('INVALID_TOKEN')
+        const InvalidAuthorization = `Bearer ${invalidToken}`
+        const zipCode = '70040010'
         const url = endpointCep + zipCode
-        cy.api_returnZipCodeData(httpMethodGet, url, failOnStatusCode, authorization)
+        cy.api_returnZipCodeData(httpMethodGet, url, failOnStatusCode, InvalidAuthorization)
             .then((response) => {
                 expect(response.status).to.eq(httpStatusUnauthorized)
                 expect(response.statusText).to.eq(statusTextUnauthorized)
@@ -54,14 +52,14 @@ describe('ZIP Code Query API', () => {
     })
 
     it('Returns ZIP code data without authorization', () => {
-        const apiToken = Cypress.env('TOKEN_ONLY_FOR_THE_QR_CODE_GENERATOR_API')
-        const authorization = `Bearer ${apiToken}`
-        const zipCode = '09691000'
+        const qrCodeGenerationApiToken = Cypress.env('QR_CODE_GENERATOR_API_TOKEN')
+        const authorizationForTheQrCodeGenerationApi = `Bearer ${qrCodeGenerationApiToken}`
+        const zipCode = '80010000'
         const url = endpointCep + zipCode
         const httpStatusForbidden = 403
         const statusTextForbidden = 'Forbidden'
         const messageForbidden = 'Este token não pode chamar a API cep.'
-        cy.api_returnZipCodeData(httpMethodGet, url, failOnStatusCode, authorization)
+        cy.api_returnZipCodeData(httpMethodGet, url, failOnStatusCode, authorizationForTheQrCodeGenerationApi   )
             .then((response) => {
                 expect(response.status).to.eq(httpStatusForbidden)
                 expect(response.statusText).to.eq(statusTextForbidden)
@@ -74,31 +72,31 @@ describe('ZIP Code Query API', () => {
      * Observation: Check if the HTTP status code should be 400 instead of 422
      */
     it('Return ZIP code data without sending ZIP code parameter', () => {
-        const apiToken = Cypress.env('TOKEN_FOR_ALL_APIS')
-        const authorization = `Bearer ${apiToken}`
         const zipCode = null
         const url = endpointCep + zipCode
-        cy.api_returnZipCodeData(httpMethodGet, url, failOnStatusCode, authorization)
+        const mandatoryFieldMessage = 'O campo cep é obrigatório.'
+        cy.api_returnZipCodeData(httpMethodGet, url, failOnStatusCode, authorizationForTheZipCodeQueryApi)
             .then((response) => {
                 expect(response.status).to.eq(httpStatusUnprocessableEntity)
                 expect(response.statusText).to.eq(statusTextUnprocessableEntity)
                 expect(response.body).to.have.property(messageObject)
-                expect(response.body.message).to.eq(messageUnprocessableEntity)
+                expect(response.body.message).to.eq(mandatoryFieldMessage)
             })
     })
 
+    /**
+     * Observation: Check if the HTTP status code should be 400 instead of 422
+     */
     it('Return ZIP code data by sending a ZIP code with less than eight digits', () => {
-        const apiToken = Cypress.env('TOKEN_FOR_ALL_APIS')
-        const authorization = `Bearer ${apiToken}`
-        const zipCode = '0969000'
+        const zipCode = '4002000'
         const url = endpointCep + zipCode
-        requestResponseBodyMessage = 'O campo cep deve conter 8 caracteres.'
-        cy.api_returnZipCodeData(httpMethodGet, url, failOnStatusCode, authorization)
+        const missingCharacterFieldMessage = 'O campo cep deve conter 8 caracteres.'
+        cy.api_returnZipCodeData(httpMethodGet, url, failOnStatusCode, authorizationForTheZipCodeQueryApi)
             .then((response) => {
                 expect(response.status).to.eq(httpStatusUnprocessableEntity)
                 expect(response.statusText).to.eq(statusTextUnprocessableEntity)
                 expect(response.body).to.have.property(messageObject)
-                expect(response.body.message).to.eq(requestResponseBodyMessage)
+                expect(response.body.message).to.eq(missingCharacterFieldMessage)
             })
     })
 
