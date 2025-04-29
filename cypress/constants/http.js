2847ccb9d89ba4c865bcdb59a4b9cb3f52416350
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2025-04-28 23:37:03 -0300

    feat: Adicionando constantes para ser utilizado por todos os arquivos

---
+++ cypress/constants/http.js
@@ -0,0 +1,20 @@
+export const HttpMethod = {
+    GET: 'GET',
+    POST: 'POST'
+}
+  
+export const HttpStatusCode = {
+    OK: 200,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_ALLOWED: 405,
+    UNPROCESSABLE_ENTIYY: 422
+}
+
+export const HttpMessage = {
+    OK: 'OK',
+    UNAUTHORIZED: 'Unauthorized',
+    FORBIDDEN: 'Forbidden',
+    NOT_ALLOWED: 'Method Not Allowed',
+    UNPROCESSABLE_ENTIYY: 'Unprocessable Entity'
+}
