bf4474bebc32bdf6128cd10356347bd8dbc38e84
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2025-05-08 08:18:19 -0300

    feat: Adicionando constantes para status HTTP 404 não encontrado

--- cypress/constants/http.js
+++ cypress/constants/http.js
@@ -7,6 +7,7 @@ export const HttpStatus = {
     OK: 200,
     UNAUTHORIZED: 401,
     FORBIDDEN: 403,
+    NOT_FOUND: 404,
     NOT_ALLOWED: 405,
     UNPROCESSABLE_ENTIYY: 422
 }
@@ -15,6 +16,7 @@ export const HttpStatusText = {
     OK: 'OK',
     UNAUTHORIZED: 'Unauthorized',
     FORBIDDEN: 'Forbidden',
+    NOT_FOUND: 'Not Found',
     NOT_ALLOWED: 'Method Not Allowed',
     UNPROCESSABLE_ENTIYY: 'Unprocessable Entity'
 }
