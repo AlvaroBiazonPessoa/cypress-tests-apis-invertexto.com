b4494fe375157e6191bbebf8a81116aca5602bcc
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2025-05-09 07:37:23 -0300

    feaat: Adicionando comando customizável

--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -16,3 +16,15 @@ Cypress.Commands.add('api_makeRequestWithoutAuthentication', (method, url, failO
         failOnStatusCode: failOnStatusCode
     })
 })
+
+Cypress.Commands.add('api_makeRequestWithQueryParameter', (method, url, failOnStatusCode, authorization, queryParameter) => {
+    cy.api({
+        method: method,
+        url: url,
+        failOnStatusCode: failOnStatusCode,
+        headers: {
+            Authorization: authorization
+        },
+        qs: queryParameter
+    })
+})
