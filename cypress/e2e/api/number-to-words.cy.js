d469da7d2d40f2cc6f8e90dcfdb8a53084ae5a08
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2025-05-09 07:42:23 -0300

    feat: Adicionando caso de teste/bloco it

--- cypress/e2e/api/number-to-words.cy.js
+++ cypress/e2e/api/number-to-words.cy.js
@@ -1,3 +1,25 @@
+import { HttpMethod, HttpStatus, HttpStatusText } from '../../constants/http'
+
 describe('Number to Words API', { env: { hideCredentials: true } }, () => {
+
+    const endpointNumberToWords = 'number-to-words' 
+    const url = endpointNumberToWords
+    const allowsErrorStatusCode = false
+    const numberToWordsApiToken = Cypress.env('NUMBER_TO_WORDS_API_TOKEN')
+    const authorizationForTheNumberToWordsApi = `Bearer ${numberToWordsApiToken}`
+
+    it('Return the number in full with an unexpected HTTP method', () => {
+        const queryParameter = {
+            number: '250',
+            language: 'pt',
+            currency: ''
+        }
+        cy.api_makeRequestWithQueryParameter(HttpMethod.PATCH, url, allowsErrorStatusCode, authorizationForTheNumberToWordsApi, queryParameter)
+            .then((response) => {
+                expect(response.status).to.eq(HttpStatus.NOT_ALLOWED)
+                expect(response.statusText).to.eq(HttpStatusText.NOT_ALLOWED)
+            }
+        )
+    })
 
 })
