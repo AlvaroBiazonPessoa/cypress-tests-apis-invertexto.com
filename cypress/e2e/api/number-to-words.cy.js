4d779ececf816d05edbcc7f5af50daa3e50f346f
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2025-05-09 22:28:24 -0300

    feat: Adicionando caso de teste/bloco it

--- cypress/e2e/api/number-to-words.cy.js
+++ cypress/e2e/api/number-to-words.cy.js
@@ -71,4 +71,17 @@ describe('Number to Words API', { env: { hideCredentials: true } }, () => {
         )
     })
 
+    it('Return the number in full without sending the mandatory parameters', () => {
+        const queryParameter = null
+        const mandatoryParametersMessage = 'Os campos number e language são obrigatórios.'
+        cy.api_makeRequestWithQueryParameter(HttpMethod.GET, endpointNumberToWords, allowsErrorStatusCode, authorizationForTheNumberToWordsApi, queryParameter)
+            .then((response) => {
+                expect(response.status).to.eq(HttpStatus.UNPROCESSABLE_ENTIYY)
+                expect(response.statusText).to.eq(HttpStatusText.UNPROCESSABLE_ENTIYY)
+                expect(response.body).to.have.property(keyMessage)
+                expect(response.body.message).to.eq(mandatoryParametersMessage)
+            }
+        )
+    })
+
 })
