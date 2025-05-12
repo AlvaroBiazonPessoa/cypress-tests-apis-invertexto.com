bac545e2dd5f44f24cb16fdbff398ddf96175629
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2025-05-12 13:57:18 -0300

    feat: Adicionando arquivo e classe Number

---
+++ cypress/fixtures/Number.js
@@ -0,0 +1,12 @@
+class Number {
+
+    constructor(number, numberInFull=null, language=null, currency=null) {
+        this.number = number
+        this.numberInFull = numberInFull
+        this.language = language
+        this.currency = currency
+    }
+
+}
+
+module.exports = Number
