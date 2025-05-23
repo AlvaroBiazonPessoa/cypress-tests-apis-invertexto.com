d368ebf1636619c5baeb3456a082e9c3d4790ecd
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2025-05-22 22:43:04 -0300

    feat: Adicionando caso de teste/bloco it

--- cypress/e2e/api/number-to-words.cy.js
+++ cypress/e2e/api/number-to-words.cy.js
@@ -241,4 +241,20 @@ describe('Number to Words API', { env: { hideCredentials: true } }, () => {
         )
     })
 
+    it('Return the number in full by sending the Azerbaijani language', { tags: ['@ID-15', '@data'] }, () => {
+        const number = new Number('12', 'on iki', 'az')
+        const queryParameter = {
+            number: number.number,
+            language: number.language
+        }
+        cy.api_makeRequestWithQueryParameter(HttpMethod.GET, url, doesNotAllowErrorStatusCode, authorizationForTheNumberToWordsApi, queryParameter)
+            .then((response) => {
+                expect(response.status).to.eq(HttpStatus.OK)
+                expect(response.statusText).to.eq(HttpStatusText.OK)
+                expect(response.body).to.have.property(keyText)
+                expect(response.body.text).to.eq(number.numberInFull)
+            }
+        )
+    })
+
 })
